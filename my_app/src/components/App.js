9eec86660c2b4ab8d1d3a22d053a9b81e5e1110d
Nick Weiland <njweiland23@gmail.com>
2023-12-04 12:29:12 -0600

    update components

--- my_app/src/components/App.js
+++ my_app/src/components/App.js
@@ -1,9 +1,12 @@
-
-import './App.css';
+import React from 'react';
+import Home from './Home.js'
+import About from './About.js';
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <About />
     </div>
   );
 }
--- my_app/src/components/Home.js
+++ my_app/src/components/Home.js
@@ -1,9 +1,19 @@
-
-import './home.css';
+import React, { useState } from 'react';
+import Search from './Search.js'
+import NewJeanForm from './NewJeanForm.js'
+import JeanList from './JeanList.js';
 
 function Home() {
+  const [jeans, setJeans] = useState([])
+
   return (
     <div className="Home">
+      <title>JEANSY</title>
+      <div>
+        <Search />
+        <NewJeanForm />
+        <JeanList />
+      </div>
     </div>
   );
 }
