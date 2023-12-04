965b2f8c4fbda67dd0786e408ccad8bf7f73c9c1
jennipher <jennipher.tejeda@aol.com>
2023-12-04 18:42:14 -0500

    updated search

--- my_app/src/components/Home.js
+++ my_app/src/components/Home.js
@@ -6,6 +6,7 @@ import JeanList from './JeanList.js';
 function Home() {
   const url = "http://localhost:4000/jeans"
   const [jeans, setJeans] = useState([])
+  const [search , setSearch] = useState("")
 
   useEffect(() => {
     loadJeans()
@@ -21,16 +22,22 @@ function Home() {
     setJeans([...jeans, newJean])
   }
  
+  function updateSearch ( e ) {
+    setSearch (e.target.value);
+
+  }
+
 
   return (
     <div className="Home">
       <div> 
         <h1> Jeansy </h1>
         <NewJeanForm url={url} addNewJean={addNewJean} />
-        <Search />
-        <JeanList jeans={jeans}/>
+        <Search search = {search} updateSearch = {updateSearch}/>
+        <JeanList jeans={jeans} search = {search}/>
       </div>
     </div>
+  
   );
 }
 
--- my_app/src/components/JeanList.js
+++ my_app/src/components/JeanList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import JeanCard from "./JeanCard";
 
-function JeanList({jeans}) {
+
+
+function JeanList({jeans, search}) {
+  const filteredJeans = jeans.filter((jean) =>{
+  return(
+    jean.name.toLowerCase().includes(search.toLowerCase()))})
   return (
-    <ul className="cards">{/* render JeanCards components in here */}
-      {jeans.map((jean)=>
+    <ul className="cards">
+      {filteredJeans.map((jean)=>
       <JeanCard key={jean.id} jean={jean} />
       )}
     </ul>
