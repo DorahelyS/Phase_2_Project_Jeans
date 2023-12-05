0c1d7c47766d9bf2f5bdac9b9bdc8824e4e18b1a
Nick Weiland <njweiland23@gmail.com>
2023-12-05 15:55:55 -0600

    css

--- my_app/src/components/About.js
+++ my_app/src/components/About.js
@@ -8,7 +8,7 @@ function About() {
         <NavBar />
       </header>
       <p>
-      Jeans is a direct to consumer website to help sell the most authentic Levi's jeans. Patnering with Levi's we are able to get their excess premium inventory whether it be returned items, samples, or simply just excess and sell it to the user at a discounted price. User is also able to submit a pair of unwanted Levi's to donate. By shopping with us, the user is helping reduce landfill waste and is guaranteed quality premium denim from the most beloved denim company!
+      Jeans is a direct to consumer website to help sell the most authentic Levi's jeans. Partnering with Levi's we are able to get their excess premium inventory whether it be returned items, samples, or simply just excess and sell it to the user at a discounted price. User is also able to submit a pair of jeans to the inventory list. By shopping with us, the user is helping reduce landfill waste and is guaranteed quality premium denim from the most beloved denim company!
       </p>
     </div>
   );
